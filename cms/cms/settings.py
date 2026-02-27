8abb154e26ea3cc0c9b164f978b3bd7367055909
Sadhon Kumar Dev <skdsadhon@gmail.com>
2026-02-27 13:01:46 +0600

    fix allowed hosts

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['course-management-system-1ql6.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
